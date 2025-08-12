05ef2a74cc4374fb99898de08b983c83c6afe4a4
David Gruen <david.gruen.git@gmail.com>
2025-08-12 17:28:10 +0200

    optimize regs

--- project_fpga-lab-final/project_fpga-lab-final.srcs/sources_1/new/perceptron.sv
+++ project_fpga-lab-final/project_fpga-lab-final.srcs/sources_1/new/perceptron.sv
@@ -23,19 +23,19 @@
 module perceptron#(
     parameter int DATA_WIDTH = 8,
     parameter int NUM_INPUTS = 8,
-    parameter int SUM_WIDTH = 32
+    parameter int OUTPUT_WIDTH = 32
 ) (
     input clk, rst, valid,
     input signed [DATA_WIDTH-1:0] inputs  [NUM_INPUTS-1:0],
     input signed [DATA_WIDTH-1:0] weights [NUM_INPUTS-1:0],
     input signed [DATA_WIDTH-1:0] bias,
-    output reg signed [SUM_WIDTH-1:0] out,
+    output reg signed [OUTPUT_WIDTH-1:0] out,
     output reg done
 );
     localparam LEVELS = $clog2(NUM_INPUTS);
     localparam P2_NUM_INPUTS = 1<<LEVELS;
 
-    reg signed [SUM_WIDTH-1:0] stage_reg [0:LEVELS] [P2_NUM_INPUTS-1:0];
+    reg signed [OUTPUT_WIDTH-1:0] stage_regs [2*P2_NUM_INPUTS-1:0];
     reg valid_stage [0:LEVELS];
 
     // mult stage
@@ -45,9 +45,9 @@ module perceptron#(
         end else begin
             for (int i = 0; i < P2_NUM_INPUTS; i++) begin : MULT_STAGE
                 if (i < NUM_INPUTS) begin
-                    stage_reg[0][i] <= $signed(inputs[i]) * $signed(weights[i]);
+                    stage_regs[i] <= $signed(inputs[i]) * $signed(weights[i]);
                 end else begin
-                    stage_reg[0][i] <= 0;
+                    stage_regs[i] <= 0;
                 end
             end
             valid_stage[0] <= valid;
@@ -57,13 +57,18 @@ module perceptron#(
     // generate pipeline stages for the adder tree
     generate
         for (genvar level = 1; level <= LEVELS; level = level + 1) begin: GEN_ADDER_TREE
-            int num_adders = P2_NUM_INPUTS / (1 << level);
+            localparam num_adders = P2_NUM_INPUTS / (1 << level);
+            
+            wire signed [SUM_WIDTH-1:0] prev_stage [2*num_adders-1:0] = stage_regs[2*P2_NUM_INPUTS - 4*num_adders +: 2*num_adders];
+            //wire signed [SUM_WIDTH-1:0] out_stage [num_adders-1:0] = stage_regs[2*P2_NUM_INPUTS - 2*num_adders +: num_adders];
+            
             always @(posedge clk) begin
                 if (rst == 1) begin
                     valid_stage[level] <= 0;
                 end else begin
                     for (int n = 0; n < num_adders; n = n + 1) begin
-                        stage_reg[level][n] <= $signed(stage_reg[level-1][2*n]) + $signed(stage_reg[level-1][2*n+1]);
+                        stage_regs[2*P2_NUM_INPUTS - 2*num_adders + n] <=
+                            $signed(prev_stage[2*n]) + $signed(prev_stage[2*n+1]);
                     end
                     valid_stage[level] <= valid_stage[level-1];
                 end
@@ -76,7 +81,7 @@ module perceptron#(
         if (rst == 1) begin
             done <= 0;
         end else begin
-            out <= $signed(stage_reg[LEVELS][0]) + $signed(bias);
+            out <= $signed(stage_regs[2*P2_NUM_INPUTS-2]) + $signed(bias);
             done <= valid_stage[LEVELS];
         end
     end
